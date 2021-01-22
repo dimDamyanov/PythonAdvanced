bb4098802d60a6db28c0d3ed79b677ec0c35e31b
Dimitar Damyanov <dim.damianov@gmail.com>
2021-01-22 23:59:17 +0200

    Edit file

--- 4. COMPREHENSION/EXERCISE/10_matrix_modification.py
+++ 4. COMPREHENSION/EXERCISE/10_matrix_modification.py
@@ -1,4 +1,25 @@
 END_COMMAND = 'END'
+ADD_COMMAND = 'Add'
+SUBTRACT_COMMAND = 'Subtract'
 
 n = int(input())
-matrix = []
+matrix = [list(map(int, input().split())) for _ in range(n)]
+while True:
+    command = input()
+    if command == END_COMMAND:
+        break
+    else:
+        args = command.split()
+        row = int(args[1])
+        col = int(args[2])
+        value = int(args[3])
+        if 0 <= row < n and 0 <= col < n:
+            if args[0] == ADD_COMMAND:
+                matrix[row][col] += value
+            elif args[0] == SUBTRACT_COMMAND:
+                matrix[row][col] -= value
+        else:
+            print('Invalid coordinates')
+
+for row in matrix:
+    print(' '.join(str(num) for num in row))
