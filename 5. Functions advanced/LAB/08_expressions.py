4a6fca57855cca654e3c7d85bac3d2cf85f55289
Dimitar I. Damyanov <dim.damianov@gmail.com>
2021-01-30 00:32:43 +0200

    Edit file

--- 5. Functions advanced/LAB/08_expressions.py
+++ 5. Functions advanced/LAB/08_expressions.py
@@ -1,1 +1,10 @@
-# TODO: https://judge.softuni.bg/Contests/Practice/Index/1838#7
+def expressions(nums, current_sum=0, expression=''):
+    if not nums:
+        return [(expression, current_sum)]
+    result_plus = expressions(nums[1:], current_sum+nums[0], f'{expression}+{nums[0]}')
+    result_minus = expressions(nums[1:], current_sum-nums[0], f'{expression}-{nums[0]}')
+    return result_plus + result_minus
+
+
+nums = [int(el) for el in input().split(', ')]
+print(*[f'{exp}={res}' for exp, res in expressions(nums)], sep='\n')
